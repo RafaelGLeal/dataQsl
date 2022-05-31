dff22b7200e27c8a12178daf8063e2e226006dcd
Rafael G. Leal <rafael.gms.leal@gmail.com>
2022-05-30 23:36:01 -0300

    up

--- src/index.js
+++ src/index.js
@@ -44,3 +44,25 @@ logoSlider[3].src = logoArray[3];
 
 loader();
 toggleMode();
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("b5lSr9BjAbz8bQdpY");
+})();
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_cirdjbl", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
